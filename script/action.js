afd216c589a71e10007b6072a67d34cfabfca3ce
ye004003 <dprdpr0000@gmail.com>
2026-02-10 16:14:35 +0900

    Add files via upload

---
+++ script/action.js
@@ -0,0 +1,8 @@
+ // (누구를).~할때(실행해라(){}
+        //     (누구를).보여라
+        // )
+
+        $('header .more').click(function(){
+            $('header .more').toggleClass('on')
+            $('.brand').fadeToggle()
+        })
